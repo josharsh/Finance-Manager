4ebe8082eb17fbcce9cc42591b3aa5b08fbef6f7
Jacob Molland <jacobmolland28@gmail.com>
2020-05-01 21:41:46 -0500

    added gitignore and converted to AndroidX for material UI

--- app/src/main/java/com/cmt/taxcalculator/FinalIncomeActivity.java
+++ app/src/main/java/com/cmt/taxcalculator/FinalIncomeActivity.java
@@ -1,10 +1,9 @@
 package com.cmt.taxcalculator;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.TextView;
 
--- app/src/main/java/com/cmt/taxcalculator/SplashScreen.java
+++ app/src/main/java/com/cmt/taxcalculator/SplashScreen.java
@@ -2,7 +2,7 @@ package com.cmt.taxcalculator;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
